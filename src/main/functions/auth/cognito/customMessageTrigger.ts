831354719c14de59236fcc63181b563c2a0bf6a3
Andrey Barreto <abarreto@ai4medimaging.com>
2025-07-28 17:38:00 +0100

    Implement email templates for password recovery functionality; add TailwindConfig component and ForgotPassword template, and update customMessageTrigger to render the new email template with confirmation code.

--- src/main/functions/auth/cognito/customMessageTrigger.ts
+++ src/main/functions/auth/cognito/customMessageTrigger.ts
@@ -1,14 +1,13 @@
 import { CustomMessageTriggerEvent } from "aws-lambda";
+import { render } from "@react-email/render";
+import ForgotPassword from "@infra/emails/templates/ForgotPassword";
 
 export async function handler(event: CustomMessageTriggerEvent) {
     if (event.triggerSource === "CustomMessage_ForgotPassword") {
         const code = event.request.codeParameter;
+        const html = await render(ForgotPassword({ confirmationCode: code }));
         event.response.emailSubject = "Reset your password";
-        event.response.emailMessage = `
-            <p>Hello,</p>
-            <p>You have requested to reset your password. Please use the following code to reset your password:</p>
-            <p>${code}</p>
-        `;
+        event.response.emailMessage = html;
     }
 
     return event;
